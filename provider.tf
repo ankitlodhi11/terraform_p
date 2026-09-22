aea2afc1ec3aca611c5eb51bf3a2f388369dd692
Ankit Lodhi <ankitlodhi11@gmail.com>
2026-09-22 15:36:35 +0530

    ok my end

--- provider.tf
+++ provider.tf
@@ -2,13 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.70.0"
+      version = "=5.0.0"
     }
   }
-}
+  backend "azurerm" {
+    resource_group_name  = "rg-statefile"
+    storage_account_name = "stagefile123321"
+    container_name       = "tfstate"
+    key                  = "dev.terraform"
 
+  }
+}
 provider "azurerm" {
   features {}
   subscription_id = "48d1c3d5-8796-4a68-8d00-c1d89903818f"
-
 }
